5b59c9e1a67c733a1564a14b4235dc7c0bf5427f
meisl <kling@sprax.org>
2021-06-21 20:11:39 +0200

    + add test class for compiler: try compiling examples

---
+++ compiler/test/examples/TestCommonExamples.kt
@@ -0,0 +1,40 @@
+package prog8tests
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import prog8.compiler.compileProgram
+import prog8.compiler.target.Cx16Target
+import kotlin.test.*
+import kotlin.io.path.*
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TestCommonExamples {
+    val workingDir = Path("").absolute()    // Note: Path(".") does NOT work..!
+    val examplesDir = Path("..", "examples")
+
+    @Test
+    fun testCurrentAndExamplesDir() {
+        assertEquals("compiler", workingDir.fileName.toString())
+        assertTrue(examplesDir.isDirectory(), "sanity check; should be directory: $examplesDir")
+    }
+
+    @Test
+    fun test_cxLogo() {
+        val filepath = examplesDir.resolve("cxlogo.p8")
+
+        val result = compileProgram(
+            filepath,
+            optimize = false,
+            writeAssembly = true,
+            slowCodegenWarnings = false,
+            compilationTarget = Cx16Target.name,
+            libdirs = listOf(),
+            outputDir = workingDir
+        )
+
+        assertTrue(result.success, "compilation successful")
+
+        //assertFailsWith<NoSuchFileException> { SourceCode.fromResources(full) }
+    }
+}
