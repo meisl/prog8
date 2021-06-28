4f0caca760d16ae3fc80786f82def901862fa8b7
meisl <kling@sprax.org>
2021-06-28 13:57:46 +0200

    + test compiler on more examples - turns out optimization is broken now

--- compiler/test/examples/TestCommonExamples.kt
+++ compiler/test/examples/TestCommonExamples.kt
@@ -7,35 +7,66 @@ import kotlin.io.path.*
 
 import prog8.compiler.target.Cx16Target
 import prog8.compiler.compileProgram
+import prog8.compiler.target.ICompilationTarget
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestCommonExamples {
     val workingDir = Path("").absolute()    // Note: Path(".") does NOT work..!
-    val examplesDir = Path("..", "examples")
+    val examplesDir = workingDir.resolve("../examples")
+    val outputDir = workingDir.resolve("build/tmp/test")
 
     @Test
-    fun testCurrentAndExamplesDir() {
+    fun testDirectoriesSanityCheck() {
         assertEquals("compiler", workingDir.fileName.toString())
         assertTrue(examplesDir.isDirectory(), "sanity check; should be directory: $examplesDir")
+        assertTrue(outputDir.isDirectory(), "sanity check; should be directory: $outputDir")
+    }
+
+
+    fun testExample(nameWithoutExt: String, platform: ICompilationTarget, optimize: Boolean) {
+        val filepath = examplesDir.resolve("$nameWithoutExt.p8")
+        val result = compileProgram(
+            filepath,
+            optimize,
+            writeAssembly = true,
+            slowCodegenWarnings = false,
+            compilationTarget = platform.name,
+            libdirs = listOf(),
+            outputDir
+        )
+        assertTrue(result.success, "${platform.name}, optimize=$optimize: \"$filepath\"")
+    }
+
+
+    @Test
+    fun test_cxLogo_noopt() {
+        testExample("cxlogo", Cx16Target, false)
+    }
+    @Test
+    fun test_cxLogo_opt() {
+        TODO("make assembly stage testable - currently assembling this with 64tass fails and Process.exit s")
+        testExample("cxlogo", Cx16Target, true)
     }
 
     @Test
-    fun test_cxLogo() {
-        val filepath = examplesDir.resolve("cxlogo.p8")
-
-        val result = compileProgram(
-            filepath,
-            optimize = false,
-            writeAssembly = true,
-            slowCodegenWarnings = false,
-            compilationTarget = Cx16Target.name,
-            libdirs = listOf(),
-            outputDir = workingDir
-        )
-
-        assertTrue(result.success, "compilation successful")
-
-        //assertFailsWith<NoSuchFileException> { SourceCode.fromResources(full) }
+    fun test_swirl_noopt() {
+        testExample("swirl", Cx16Target, false)
     }
+    @Test
+    fun test_swirl_opt() {
+        TODO("make assembly stage testable - currently assembling this with 64tass fails and Process.exit s")
+        testExample("swirl", Cx16Target, true)
+    }
+
+    @Test
+    fun test_animals_noopt() {
+        testExample("animals", Cx16Target, false)
+    }
+    @Test
+    fun test_animals_opt() {
+        TODO("make assembly stage testable - currently assembling this with 64tass fails and Process.exit s")
+        testExample("animals", Cx16Target, true)
+    }
+
 }
